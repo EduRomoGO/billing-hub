6595633eff96715e5d6c2f0ae164a2261e8ad08d
EduRomoGO <eduromogo@outlook.es>
2020-02-15 16:13:02 +0000

    feat: add tests

--- src/components/BillingHub/BillingHub.js
+++ src/components/BillingHub/BillingHub.js
@@ -9,7 +9,9 @@ const BillingHub = () => {
 
   useEffect(() => {
     axios.get('http://interview.dekopay.com.s3.eu-west-2.amazonaws.com/merchants.json')
-      .then(({data}) => setMerchants(data))
+      .then(({data}) => {
+        setMerchants(data);
+      })
       .catch(err => { throw new Error(err) });
 
   }, []);
@@ -20,11 +22,18 @@ const BillingHub = () => {
       .catch(err => { throw new Error(err) });
   }
 
+  const renderMerchantList = merchants => {
+    return merchants.map(({ name, merchant_id }) => <li className='b-merchant-list__item' onClick={() => handleMerchantClick(merchant_id)} key={merchant_id}>{name}</li>);
+  }
+
   return <section className='c-billing-hub'>
     <article className='c-billing-hub__aside'>
+      <header>
+        <h1>hola</h1>
+      </header>
       <nav className='c-billing-hub__nav'>
         <ul className='b-merchant-list'>
-          {merchants.slice(0, 6).map(({ name, merchant_id }) => <li className='b-merchant-list__item' onClick={() => handleMerchantClick(merchant_id)} key={merchant_id}>{name}</li>)}
+          {renderMerchantList(merchants)}
         </ul>
       </nav>
     </article>

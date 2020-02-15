01dd3bef3f53e4ec46cda622a04597019f194278
EduRomoGO <eduromogo@outlook.es>
2020-02-15 09:33:56 +0000

    feat: print list

--- src/components/BillingHub/BillingHub.js
+++ src/components/BillingHub/BillingHub.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './BillingHub.css';
 
+
+
+// http://interview.dekopay.com.s3.eu-west-2.amazonaws.com/merchants/A1201.json
+
 const BillingHub = () => {
+  const [merchants, setMerchants] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://interview.dekopay.com.s3.eu-west-2.amazonaws.com/merchants.json')
+      .then(data => setMerchants(data.data))
+      .catch(err => { throw new Error(err) });
+
+  }, []);
+
   return <section className='c-billing-hub'>
-    billinghub
+    <article className='b-merchant-list'>
+      {merchants.map(({name, merchant_id}) => <li key={merchant_id}>{name}</li>)}
+    </article>
   </section>
 };
 

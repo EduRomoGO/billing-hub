f080f23cf4021a667955e33435de4de93724e100
EduRomoGO <eduromogo@outlook.es>
2020-02-15 19:44:30 +0000

    refactor: extract

--- src/components/MerchantDetails/MerchantDetails.js
+++ src/components/MerchantDetails/MerchantDetails.js
@@ -1,23 +1,17 @@
 import React from 'react';
 
+const getTotal = ({transactions}) => transactions.reduce((acc, next) => acc + next.price, 0);
+const getSubsidyAmountForPrice = (price, pricing) => {
+  return (price < pricing.discount_cutoff) ? pricing.subsidy : pricing.discount_subsidy;
+};
+const getSubsidy = ({transactions, pricing}) => transactions.reduce(
+  (acc, next) => {
+    return acc + next.price * (getSubsidyAmountForPrice(next.price, pricing) / 100);
+  },
+  0,
+);
+
 const MerchantDetails = ({ name, transactions, pricing }) => {
-  const getTotal = ({transactions}) => transactions.reduce((acc, next) => acc + next.price, 0);
-  const getSubsidyAmountForPrice = (price, pricing) => {
-    return (price < pricing.discount_cutoff) ? pricing.subsidy : pricing.discount_subsidy;
-  };
-  const getSubsidy = ({transactions, pricing}) => transactions.reduce(
-    (acc, next) => {
-      return acc + next.price * (getSubsidyAmountForPrice(next.price, pricing) / 100);
-    },
-    0,
-  );
-
-  // pricing: {
-  //   "subsidy": 10,
-  //   "discount_subsidy": 5,
-  //   "discount_cutoff": 200
-  // },
-
 
   return <article className='c-merchant-details'>
     <div className='c-merchant-details__name'>{name}</div>
